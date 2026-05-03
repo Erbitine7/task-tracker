18fb1d04c3e76704dc67d8358769f1d4ddc1fea5
Erbitine7 <sulthanardg@gmail.com>
2026-05-03 19:31:37 +0700

    Added TinyMCE style.css, Added draft capabilities

--- app/Views/home.php
+++ app/Views/home.php
@@ -5,27 +5,27 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Task Tracker</title>
     <link rel="stylesheet" href="<?= base_url('css/output.css') ?>">
+    <link rel="stylesheet" href="<?= base_url('css/style.css') ?>">
     <script>
         window.APP_CONFIG = {
             baseUrl: "<?= base_url() ?>"
         };
     </script>
     <script src="<?= base_url('tinymce/tinymce.min.js') ?>" referrerpolicy="origin" crossorigin="anonymous"></script>
-    <script src="<?= base_url('js/scripts.js') ?>"></script>
 </head>
 <body class="bg-slate-950 text-white flex flex-row justify-between">
         <!-- <h1 class="text-2xl font-bold ml-5 py-10">Tambah Tugas</h1> -->
-    <form action="#" method="POST" class="w-1/3 h-screen p-5 flex flex-col sticky top-0">
+    <form action="#" method="POST" class="w-1/3 h-screen p-10 flex flex-col sticky top-0">
         <div class="mb-5">
-            <label for="tugas" class="block mb-2 text-sm font-medium">Judul Tugas</label>
-            <input type="text" id="tugas" name="tugas" class="w-full p-2 border border-gray-700 rounded-md focus:outline-none focus:ring-2 focus:ring-lime-300">
+            <label for="tugas" class="block mb-2 text-md font-medium">Judul Tugas</label>
+            <input type="text" id="tugas" name="tugas" onchange="saveDraft()" class="w-full p-2 border bg-gray-800 border-gray-700 rounded-md focus:outline-none focus:ring-2 focus:ring-lime-300">
         </div>
         <div class="mb-5 grow flex flex-col">
-            <label for="deskripsi" class="block mb-2 text-sm font-medium">Deskripsi</label>
-            <textarea id="deskripsi" name="deskripsi"></textarea>
+            <label for="deskripsi" class="block mb-2 text-md font-medium">Deskripsi</label>
+            <textarea id="deskripsi" name="deskripsi" onchange="saveDraft()"></textarea>
         </div>
         <div class="flex justify-end gap-5">
-            <button type="reset" class=" text-red-500 font-bold py-2 px-4 cursor-pointer">
+            <button type="reset" onclick="removeDraft()" class=" text-red-500 font-bold py-2 px-4 cursor-pointer">
                 Hapus Draft
             </button>
             <button type="submit" class="bg-lime-300 hover:bg-lime-700 text-slate-950 font-bold py-2 px-4 cursor-pointer">
@@ -33,7 +33,7 @@
             </button>
         </div>
     </form>
-    <table class="table-auto w-2/3 mt-5 border-collapse mr-10 ml-5 h-min">
+    <table class="table-auto w-2/3 mt-5 border-collapse mr-10 h-min">
         <thead>
             <th class="p-5 text-left w-px">No.</th>
             <th class="p-5 text-left">Tugas</th>
@@ -542,5 +542,6 @@
             </tr>
         </tbody>
     </table>
+    <script src="<?= base_url('js/scripts.js') ?>"></script>
 </body>
 </html>
--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,14 +1,47 @@
+const tugas = document.getElementById('tugas');
+
 tinymce.init({
   selector: '#deskripsi',
   license_key: 'gpl',
+  setup: (editor) => {
+    editor.on('change', () => {
+      saveDraft();
+    });
+    editor.on('init', function() {
+      loadDraft();
+    });
+  },
   content_css: window.APP_CONFIG.baseUrl + 'css/output.css',
   skin: 'oxide-dark',
   body_class: 'bg-slate-900 text-white grow focus:none',
-  plugins: 'autosave',
-  autosave_ask_before_unload: false,
-  autosave_restore_when_empty: true,
+  promotion: false,
+  resize: false,
 });
 
+tugas.addEventListener('change', function() {
+  saveDraft();
+});
+
+function loadDraft() {
+  const draft = JSON.parse(localStorage.getItem('draft'));
+  if (draft) {
+    console.log('Draft loaded');
+    tugas.value = draft.tugas;
+    tinymce.get('deskripsi').setContent(draft.deskripsi);
+  } else {
+    console.log('No draft found');
+  }
+}
+
+function saveDraft() {
+  console.log('Draft saved');
+  localStorage.setItem('draft', JSON.stringify({
+    tugas: tugas.value,
+    deskripsi: tinymce.get('deskripsi').getContent(),
+  }));
+}
+
 function removeDraft() {
-  tinymce.get('deskripsi').setContent('');
+  console.log('Draft removed');
+  localStorage.removeItem('draft');
 }
